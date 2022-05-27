0fb79ccd6c068e4570bb8ff99539743459ddf08f
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-05-28 00:42:56 +0700

    Lesson 63: Dựng khung layouts mặc định.

--- src/Components/Layout/DefaultLayouts/index.js
+++ src/Components/Layout/DefaultLayouts/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '~/Components/Layout/Components/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayouts.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const DefaultLayout = ({ children }) => {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
