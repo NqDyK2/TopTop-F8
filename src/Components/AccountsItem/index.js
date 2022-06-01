87ec455371da64d355f30fb1c27c2af00831229a
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-06-01 15:39:13 +0700

    feat: update lesson-65 with The content of the lesson is Xây dựng UI phần Header #65.

---
+++ src/Components/AccountsItem/index.js
@@ -0,0 +1,25 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountsItem.module.scss';
+const cx = classNames.bind(styles);
+function AccountsItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/a187c4dfa896a5ea449a4c5d3927b20a~c5_300x300.webp?x-expires=1654243200&x-signature=uzfSZUiZfShC2C%2FW4Zd%2Fr0LQvl4%3D"
+                className={cx('avatar')}
+                alt="......"
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Mixigaming</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>Độ Phùng</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountsItem;
